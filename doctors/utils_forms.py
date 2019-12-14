73618b4327da9172e1d7e0f6814018c29bf6367f
Ha Thuc Phuc <hathucphuc@live.com>
2019-12-14 20:46:06 +0700

    change required fields form at doctor_medical_record page

--- doctors/utils_forms.py
+++ doctors/utils_forms.py
@@ -7,14 +7,14 @@ def clean_upload_file(file):
     if file:
         content_type = file.content_type.split("/")[1]
         if content_type in settings.CONTENT_TYPES:
-            print(file.size)
+            
             if file.size > settings.MAX_UPLOAD_SIZE:
                 raise forms.ValidationError("File upload của bạn trên 5M. Làm ơn chọn file dưới 5M!")
             else:
-                print("1")
+                
                 return file
         else:
             raise forms.ValidationError("Bạn nên upload định dạng PDF")
-    print(file)
+
     return file
     
