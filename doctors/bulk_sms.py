3b195b32a4e4ced8c8013c77a6de04c496040e39
Ha Thuc Phuc <hathucphuc@live.com>
2019-12-26 10:51:02 +0700

    fix something

--- doctors/bulk_sms.py
+++ doctors/bulk_sms.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import requests, re
 from urllib import request
+from datetime import datetime
 
 def send_sms(tenBS,maPK,soTT,ngayKham,phone):
    url = 'http://ams.tinnhanthuonghieu.vn:8009/bulkapi?wsdl'
@@ -40,9 +41,15 @@ def send_sms(tenBS,maPK,soTT,ngayKham,phone):
    body = xml.find('{http://schemas.xmlsoap.org/soap/envelope/}Body').find('{http://impl.bulkSms.ws/}wsCpMt')
 
    content = "Bạn đã đặt lịch hẹn thành công bác sỹ "+tenBS+", mã phòng khám "+str(maPK)+", số thứ tự "+soTT+ ", giờ khám " +ngayKham.strftime("%H:%M")+" ngày "+ngayKham.strftime("%d/%m")+". Cảm ơn Quý khách đã sử dụng dịch vụ của chúng tôi. Trân trọng!"
+
+   content1 = "Bạn đã đặt lịch hẹn thành công bác sỹ "+tenBS+", mã phòng khám "+str(maPK)+", số thứ tự "+soTT+ ", ngày "+ngayKham.strftime("%d/%m")+". Cảm ơn Quý khách đã sử dụng dịch vụ của chúng tôi. Trân trọng!"
+   now = datetime.now()
    # content1 = "Ban da dat lich hen thanh cong phong kham bac si Nguyen Van A, ma phong kham x, so thu tu xx, gio kham hh:mm ngay dd/mm"+ngayKham.strftime("%d/%m/%Y")
    # print(type(body.find("UserID").text))
-   body.find("Content").text = content
+   if now < ngayKham:
+      body.find("Content").text = content
+   else:
+      body.find("Content").text = content1
    body.find("UserID").text = phone
    body.find("ReceiverID").text = phone
    # print(body.find("User").text)
