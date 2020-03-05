c9c52001f6dd0d6976b112f269c4b40aea2bbe02
Ha Thuc Phuc <hathucphuc@live.com>
2020-03-05 23:06:34 +0700

    fixed something

--- user/vnpay/urls_vnpay.py
+++ user/vnpay/urls_vnpay.py
@@ -5,7 +5,7 @@ import user.vnpay.views_vnpay
 urlpatterns = [
     url(r'^$', user.vnpay.views_vnpay.index, name='index'),
     url(r'^payment$', user.vnpay.views_vnpay.payment, name='payment'),
-    url(r'^payment_ipn$', user.vnpay.views_vnpay.payment_ipn, name='payment_ipn'),
+    url(r'^sfc-vnpay/payment_ipn$', user.vnpay.views_vnpay.payment_ipn, name='payment_ipn'),
     url(r'^payment_return$', user.vnpay.views_vnpay.payment_return, name='payment_return'),
     url(r'^query$', user.vnpay.views_vnpay.query, name='query'),
     url(r'^refund$', user.vnpay.views_vnpay.refund, name='refund'),
